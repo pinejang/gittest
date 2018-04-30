143b2dcfbc4e95dcdf7629aae0288aeecdcc4c2f
Sin <Sin@DESKTOP-D3CTAQ3>
2018-04-30 10:06:33 +0900

    123

--- src/main/java/com/example/gitTest.java
+++ src/main/java/com/example/gitTest.java
@@ -4,7 +4,7 @@ public class gitTest {
 	
 	public static void main(String[] args) {
 		System.out.println("안녕 깃허브123");
-		System.out.println("메시지 확인 완료");
+		System.out.println("메시지 확인 완료123");
 	}
 
 }
